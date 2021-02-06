285167d1dbea4759a19845f5e7f60f308cf8cde6
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-02-06 10:32:12 -0300

    reorganize mutation types

--- GraphQL/Speakers/SpeakerMutations.cs
+++ GraphQL/Speakers/SpeakerMutations.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using ConferencePlanner.GraphQL.Data;
 using HotChocolate;
+using HotChocolate.Types;
 
-namespace ConferencePlanner.GraphQL
+namespace ConferencePlanner.GraphQL.Speakers
 {
-    public class Mutation
+    [ExtendObjectType(Name = "Mutation")]
+    public class SpeakerMutations
     {
         [UseApplicationDbContext]
         public async Task<AddSpeakerPayload> AddSpeakerAsync(
